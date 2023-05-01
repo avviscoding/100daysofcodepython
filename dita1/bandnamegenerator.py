6f9940c8e37e3c5f086d2d6d77c17f96f72cbe5e
avviscoding <132305050+avviscoding@users.noreply.github.com>
2023-05-01 23:01:05 +0200

    Add files via upload

---
+++ dita1/bandnamegenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city = input("Which city did you grew up in ?")
+pet = input("What is your pet name?")
+print("Your brand name could be " + city +" " + pet)
