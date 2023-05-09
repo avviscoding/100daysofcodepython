a1ed9c045acb3faf29dd2b292461746d8b04b0b8
avviscoding <132305050+avviscoding@users.noreply.github.com>
2023-05-09 18:35:08 +0200

    Add files via upload

---
+++ dita9/dictionairy.py
@@ -0,0 +1,17 @@
+programming_dictionary = {
+ "Bug": "An error in a program that prevents the program from running as expected.",
+ "Functions": "A piece of code that you can easily call over and over again.",
+ "Loop": "The action of doing soemthing over and over again.",
+}
+
+print(programming_dictionary["Bug"])
+# adding new items
+programming_dictionary["Hava"] = "Little girl with giant dreams."
+print(programming_dictionary["Hava"])
+
+# te jep keys
+# for thing in programming_dictionary:
+#  print(thing)
+for key in programming_dictionary:
+ print(programming_dictionary[key])
+ # te jep keys  dhe ato qe ke shkr brenda disctionary
---
+++ dita9/nested lists & dictionaires.py
@@ -0,0 +1,12 @@
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
+# # nesting  a list in dictionairy
+# travel_log = [
+#     {
+#      "country:"France",
+#      "cities_visited":["Paris","Lille","Dijon"],
+#      "total_visits":12
+# },
+# print(travel_log)
